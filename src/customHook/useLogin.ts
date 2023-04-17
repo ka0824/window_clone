f9750d1136f02389454debbe46289c7dddda4f2a
ka0824 <rkgud0824@gmail.com>
2023-04-18 01:50:53 +0900

    feat: make icon
    - 아이콘의 레이아웃 (반응형 적용)
    - 개별 아이콘 자리 이동 기능
    - 개별 아이콘 이름 수정 기능
    - 개별 아이콘 삭제 기능

--- src/customHook/useLogin.ts
+++ src/customHook/useLogin.ts
@@ -5,13 +5,14 @@ function useLogin() {
 
   function handleStorage() {
     console.log(localStorage.getItem("userEmail"));
+
     setIsLogin(!!localStorage.getItem("userEmail"));
   }
 
   useEffect(() => {
     if (
       localStorage.getItem("userEmail") &&
-      localStorage.getItem("userEmail") !== ""
+      localStorage.getItem("userEmail") !== "guest"
     ) {
       setIsLogin(true);
     }
---
+++ src/data/icons.tsx
@@ -0,0 +1,7 @@
+import { AiOutlineFileText } from "react-icons/ai";
+import { IconType } from "../types";
+
+const icons: IconType = {
+  test: <AiOutlineFileText size={36}></AiOutlineFileText>,
+};
+export default icons;
