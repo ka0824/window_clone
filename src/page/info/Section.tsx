e6aaad3d11d1e713044d74efb97dca8ee35b0b2d
ka0824 <rkgud0824@gmail.com>
2023-05-03 15:41:17 +0900

    feat: change info page image
    - 소개 페이지 이미지 수정
    
    - 1초마다 이미지 달라지는 기능 추가

--- src/page/info/Section.tsx
+++ src/page/info/Section.tsx
@@ -1,16 +1,37 @@
+import { useState, useEffect } from "react";
+
 function Section({
   scrollPosition,
   standard,
   title,
   content,
-  src,
+  imgs,
 }: {
   scrollPosition: number;
   standard: number;
   title: string;
   content: string;
-  src: string;
+  imgs: string[];
 }) {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  function changeIndex(prevIndex: number) {
+    if (prevIndex >= imgs.length - 1) {
+      return 0;
+    }
+    return prevIndex + 1;
+  }
+
+  useEffect(() => {
+    const intervalIdx = setInterval(() => {
+      setImgIndex((prevIndex) => changeIndex(prevIndex));
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalIdx);
+    };
+  }, []);
+
   return (
     <section
       className={
@@ -21,8 +42,12 @@ function Section({
     >
       <div className="flex p-20 w-full mb-2 justify-center">
         <div className="bg-white rounded-xl flex py-20 w-4/5 flex-wrap">
-          <div className="flex justify-center mx-auto bg-red-50">
-            <img src={src} className="w-80 h-80 hidden sm:block"></img>
+          <div className="flex justify-center mx-auto mb-8">
+            <img
+              src={imgs[imgIndex]}
+              className="hidden sm:block px-10 w-200 h-96"
+              alt=""
+            />
           </div>
           <div className="flex flex-col justify-center items-center max-w-fit mx-auto">
             <div className="text-4xl mb-8 w-full text-center">{title}</div>
